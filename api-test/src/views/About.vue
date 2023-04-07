0a9613fd6ac23bc954cd33514c8424ef7d4d3890
Wilson <musso.luca89@gmail.com>
2023-04-07 14:31:44 +0200

    aggiunto vuetify

---
+++ api-test/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <Slide>
+    <a id="home" href="#">
+      <span>Home</span>
+    </a>
+    <button>Test</button>
+  </Slide>
+</template>
+
+<script>
+import { Slide } from "vue-burger-menu"; // import the CSS transitions you wish to use, in this case we are using `Slide`
+
+export default {
+  components: {
+    Slide, // Register your component
+  },
+};
+</script>
+
+
+
